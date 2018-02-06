6f9e7abfc7ea504758736c280b01e9740117c678
thayoz <email@kamitech.ch>
2018-02-06 22:01:14 +0100

    Initial Commit

---
+++ code/coffeeboxcounter.ino
@@ -0,0 +1,18 @@
+//
+// Sketch for the CoffeBoxCounter
+// 2018 -
+//
+
+// Included libraries
+#include <ESP8266WiFi.h>      // ESP library for all WiFi functions
+#include <ArduinoOTA.h>       // Library for uploading sketch over the air (OTA)
+#include <SPI.h>              // Library for hardware or software driven SPI
+#include <EEPROM.h>           // Library for EEPROM functions
+
+void setup() {
+
+}
+
+void loop() {
+
+}
