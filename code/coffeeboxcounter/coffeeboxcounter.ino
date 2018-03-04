d7ad8aefba4dec7a4f704fbdd35abdf2586a9dc1
thayoz <email@kamitech.ch>
2018-03-04 22:21:32 +0100

    Users totals, Users debug and push button implemented

--- code/coffeeboxcounter/coffeeboxcounter.ino
+++ code/coffeeboxcounter/coffeeboxcounter.ino
@@ -33,12 +33,29 @@
 // Included custom
 #include "animations.h"       // Header files for screen animations
 
+// Version
+#define VERS 1.0
+
+// USERS
+#define NUM_USERS 5
+// Struct to define user structure
 typedef struct {
-		byte uid[4];             //4 Bytes
-    char name[4];               // 4 Bytes
-    unsigned int current_total; // 2 Bytes
-    unsigned int backup_total;  // 2 Bytes
-} User;                         // TOTAL 12 Bytes (42 Users in 512 B EPROM)
+		byte uid[4];             //4 Bytes - UID to compare RFID respons
+    char name[4];               // 4 Bytes - User abreviation i.e: David - DAV
+    unsigned int current_total; // 2 Bytes - Current user's coffee consumed
+    unsigned int backup_total;  // 2 Bytes - Backup of the user's cofee consumption before last reset
+} user; // TOTAL 12 Bytes (42 Users in 512 B EPROM)
+
+//user users [X]; X Max 42 Users
+
+// Test Users
+user users [NUM_USERS] = {
+  {{0xFF,0xFF,0xEE,0x00},"THI",11,10},
+  {{0xAA,0xBB,0xCC,0x0A},"MAR",2,98},
+  {{0xCC,0xEE,0xDD,0xDD},"MAN",121,221},
+  {{0x01,0x22,0x33,0xFF},"ERI",59,300},
+  {{0x01,0x22,0x33,0xFF},"ROB",333,109}
+};
 
 // RFID
 //#define SS_PIN 10             // RFID slave select pin
@@ -46,21 +63,23 @@ typedef struct {
 //MFRC522 mfrc522(SS_PIN, RST_PIN);// instatiate a MFRC522 reader object.
 
 // OLED
-// SCL GPIO5
-// SDA GPIO4
 #define OLED_RESET 0  // GPIO0
 Adafruit_SSD1306 display(OLED_RESET);
 
 // Push Button
-#define PUSH_BTN_PIN 4
+#define PUSH_BTN_PIN D4
 
 // Buzzer
-#define BUZZER_PIN 2
+//#define BUZZER_PIN 2
 
 // Functions declaration
+void btn_pushed();
 void createEE();
 void saveEE();
+void splash_screen();
 void coffee_animate();
+void display_users_debug();
+void display_users_totals();
 
 void setup() {
   Serial.begin(9600);        // Initialize serial communications for serial communications over USB
@@ -71,10 +90,14 @@ void setup() {
   // OLED Setup
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
   Serial.println("OLED Screen Ready");
-  // Push BTN Setup
-  //pinMode(PUSH_BTN_PIN, INPUT);
+  // Push BTN Setup - Using interrupt to
+  attachInterrupt(PUSH_BTN_PIN, btn_pushed, RISING);
   // Buzzer Setup
   //pinMode(BUZZER_PIN, OUTPUT);
+}
+
+void btn_pushed(){
+  Serial.println("#BUTTON LOW");
 }
 
 void loop() {
@@ -96,13 +119,11 @@ void loop() {
   //}
 
   // Detects if Button is pressed
-    if (digitalRead(PUSH_BTN_PIN) == HIGH) {
-          
-    }
+    
 
       //Display the table of user with current consuptions
 
-    //3 press
+      //3 press
 
       //Wait for RFID Card with UID 0x0001
 
@@ -113,10 +134,43 @@ void loop() {
              //Copy current value to backup
 
              //Reser current counters
-  
+  splash_screen();
+  delay(2000);
+  display_users_totals();
+  delay(3000);
   //Animate Oled Screen with HotCoffee
   coffee_animate();
   delay(50);
+}
+
+void display_users_debug(){
+  // Clear the buffer.
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  for(int i = 0; i<NUM_USERS; i++){
+    display.print(users[i].name);
+    display.print("    ");
+    display.print(users[i].current_total);
+    display.print("\n");
+  }
+  display.display(); 
+}
+
+void display_users_totals(){
+  // Clear the buffer.
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  for(int i = 0; i<NUM_USERS; i++){
+    display.print(users[i].name);
+    display.print("    ");
+    display.print(users[i].current_total);
+    display.print("\n");
+  }
+  display.display();  
 }
 
 void createEE(){
@@ -127,18 +181,25 @@ void saveEE(){
   
 }
 
-void coffee_animate(){
-  display.display();
-    // Clear the buffer.
+void splash_screen(){
   display.clearDisplay();
  
   // text display tests
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
-  display.println("COFFEEBOX\nCOUNTER");
+  display.println("COFFEEBOX");
+  display.println("COUNTER\n");
+  display.print("Ver: b");
+  display.print(VERS);
   display.display();
-  delay(2000);
+}
+
+void coffee_animate(){
+  //display.display();
+    // Clear the buffer.
+  display.clearDisplay();
+  
   display.clearDisplay();
   display.drawBitmap(0, 0, anim01, 64, 48, WHITE);
   display.display();
