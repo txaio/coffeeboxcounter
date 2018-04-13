59f9c0689249584259d3ebe283ae27e90ef771e5
thayoz <email@kamitech.ch>
2018-04-13 17:41:06 +0200

    RFID Integration POC

--- code/coffeeboxcounter/coffeeboxcounter.ino
+++ code/coffeeboxcounter/coffeeboxcounter.ino
@@ -17,10 +17,10 @@
 // RX  - RXD
 // D1  - GPIO5 SCL
 // D2  - GPIO4 SDA
-// D3  - GPIO0
+// D3  - GPIO0 SPI RST
 // D4  - GPIO2
 // GND - Ground
-// 5V  - 
+// 5V  -
 
 // Included libraries
 #include <ESP8266WiFi.h>      // ESP library for all WiFi functions
@@ -58,10 +58,13 @@ user users [NUM_USERS] = {
 };
 
 // RFID
-//#define SS_PIN 10             // RFID slave select pin
-//#define RST_PIN 5             // RFID reset pin
-//MFRC522 mfrc522(SS_PIN, RST_PIN);// instatiate a MFRC522 reader object.
-
+#define SS_PIN 15             // RFID slave select pin
+#define RST_PIN 0             // RFID reset pin
+MFRC522 mfrc522(SS_PIN, RST_PIN);// instatiate a MFRC522 reader object.
+MFRC522::MIFARE_Key key;
+int block;
+byte readbackblock[18];
+byte blockcontent[16] = {"PUTINPYTHONPUTI"};
 // OLED
 #define OLED_RESET 0  // GPIO0
 Adafruit_SSD1306 display(OLED_RESET);
@@ -84,14 +87,24 @@ void display_users_totals();
 void setup() {
   Serial.begin(9600);        // Initialize serial communications for serial communications over USB
   Serial.println("### COFFE BOX COUNTER ###");
-  //SPI.begin();               // Init SPI bus
-  //mfrc522.PCD_Init();        // Init MFRC522 card (in case you wonder what PCD means: proximity coupling device)
-  //Serial.println("MFRC522 Ready");
+  SPI.begin();               // Init SPI bus
+  mfrc522.PCD_Init();        // Init MFRC522 card (in case you wonder what PCD means: proximity coupling device)
+  Serial.println("MFRC522 Ready");
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;//keyByte is defined in the "MIFARE_Key" 'struct' definition in the .h file of the library
+  }
+  block=2;//this is the block number we will write into and then read. Do not write into 'sector trailer' block, since this can make the block unusable.
+  //mfrc522.PCD_DumpVersionToSerial();
+  //byte blockcontent[16] = {"makecourse_____"};//an array with 16 bytes to be written into one of the 64 card blocks is defined
+    //blockcontent[16] = {"PUTINPYTHONPUTI"};//an array with 16 bytes to be written into one of the 64 card blocks is defined
+  //byte blockcontent[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};//all zeros. This can be used to delete a block.
+  //This array is used for reading out a block. The MIFARE_Read method requires a buffer that is at least 18 bytes to hold the 16 bytes of a block.
+
   // OLED Setup
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
-  Serial.println("OLED Screen Ready");
+  //display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+  //Serial.println("OLED Screen Ready");
   // Push BTN Setup - Using interrupt to
-  attachInterrupt(PUSH_BTN_PIN, btn_pushed, RISING);
+  //attachInterrupt(PUSH_BTN_PIN, btn_pushed, RISING);
   // Buzzer Setup
   //pinMode(BUZZER_PIN, OUTPUT);
 }
@@ -103,25 +116,28 @@ void btn_pushed(){
 }
 
 void loop() {
-   Serial.println("#LOOPING#");
-  // Detects new RFID Card
-  //if (mfrc522.PICC_IsNewCardPresent()) {
-  //  Serial.println("RFID detected");
-    // Read uuid form the RFID Card
-  //  if (mfrc522.PICC_ReadCardSerial()) {
-  //    Serial.println("ID:");
-      // UUID -> Name Lookup & Counter increment
+   //Serial.println("#LOOPING#");
+  //Detects new RFID Card
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {//if PICC_IsNewCardPresent returns 1, a new card has been found and we continue
+    return;//if it did not find a new card is returns a '0' and we return to the start of the loop
+  }
+  Serial.println("RFID detected");
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {//if PICC_ReadCardSerial returns 1, the "uid" struct (see MFRC522.h lines 238-45)) contains the ID of the read card.
+    return;//if it returns a '0' something went wrong and we return to the start of the loop
+  }
 
-      // Visual Feedback Oled
-
-      // Sound Feedback
-
-      // TIMEOUT
-   // }
-  //}
-
+  //writeBlock(block, blockcontent);
+  
+  readBlock(block, readbackblock);
+  Serial.print("read block: ");
+      for (int j=0 ; j<16 ; j++){
+        Serial.write (readbackblock[j]);//Serial.write() transmits the ASCII numbers as human readable characters to serial monitor
+      }
+  Serial.println("");
+  //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
   // Detects if Button is pressed
-    
+
 
       //Display the table of user with current consuptions
 
@@ -136,12 +152,12 @@ void loop() {
              //Copy current value to backup
 
              //Reser current counters
-  splash_screen();
-  delay(2000);
-  display_users_totals();
-  delay(3000);
+  //splash_screen();
+  //delay(2000);
+  //display_users_totals();
+  //delay(3000);
   //Animate Oled Screen with HotCoffee
-  coffee_animate();
+  //coffee_animate();
   delay(50);
 }
 
@@ -154,12 +170,12 @@ void display_users_debug(){
   for(int i = 0; i<NUM_USERS; i++){
     display.print(users[i].name);
     display.print("    ");
-    for(int y = 0; y<4; y++){ 
+    for(int y = 0; y<4; y++){
       display.print(users[i].uid[y],HEX);
     }
     display.print("\n");
   }
-  display.display(); 
+  display.display();
 }
 
 void display_users_totals(){
@@ -174,20 +190,20 @@ void display_users_totals(){
     display.print(users[i].current_total);
     display.print("\n");
   }
-  display.display();  
+  display.display();
 }
 
 void createEE(){
-  
+
 }
 
 void saveEE(){
-  
+
 }
 
 void splash_screen(){
   display.clearDisplay();
- 
+
   // text display tests
   display.setTextSize(1);
   display.setTextColor(WHITE);
@@ -203,7 +219,7 @@ void coffee_animate(){
   //display.display();
     // Clear the buffer.
   display.clearDisplay();
-  
+
   display.clearDisplay();
   display.drawBitmap(0, 0, anim01, 64, 48, WHITE);
   display.display();
@@ -225,4 +241,77 @@ void coffee_animate(){
   display.drawBitmap(0, 0, anim05, 64, 48, WHITE);
   display.display();
   }
+}
+
+int writeBlock(int blockNumber, byte arrayAddress[]) 
+{
+  //this makes sure that we only write into data blocks. Every 4th block is a trailer block for the access/security info.
+  int largestModulo4Number=blockNumber/4*4;
+  int trailerBlock=largestModulo4Number+3;//determine trailer block for the sector
+  if (blockNumber > 2 && (blockNumber+1)%4 == 0){Serial.print(blockNumber);Serial.println(" is a trailer block:");return 2;}//block number is a trailer block (modulo 4); quit and send error code 2
+  Serial.print(blockNumber);
+  Serial.println(" is a data block:");
+  
+  /*****************************************authentication of the desired block for access***********************************************************/
+  byte status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(mfrc522.uid));
+  //byte PCD_Authenticate(byte command, byte blockAddr, MIFARE_Key *key, Uid *uid);
+  //this method is used to authenticate a certain block for writing or reading
+  //command: See enumerations above -> PICC_CMD_MF_AUTH_KEY_A  = 0x60 (=1100000),    // this command performs authentication with Key A
+  //blockAddr is the number of the block from 0 to 15.
+  //MIFARE_Key *key is a pointer to the MIFARE_Key struct defined above, this struct needs to be defined for each block. New cards have all A/B= FF FF FF FF FF FF
+  //Uid *uid is a pointer to the UID struct that contains the user ID of the card.
+  if (status != MFRC522::STATUS_OK) {
+         Serial.print("PCD_Authenticate() failed: ");
+         //Serial.println(mfrc522.GetStatusCodeName(status));
+         return 3;//return "3" as error message
+  }
+  //it appears the authentication needs to be made before every block read/write within a specific sector.
+  //If a different sector is being authenticated access to the previous one is lost.
+
+
+  /*****************************************writing the block***********************************************************/
+        
+  status = mfrc522.MIFARE_Write(blockNumber, arrayAddress, 16);//valueBlockA is the block number, MIFARE_Write(block number (0-15), byte array containing 16 values, number of bytes in block (=16))
+  //status = mfrc522.MIFARE_Write(9, value1Block, 16);
+  if (status != MFRC522::STATUS_OK) {
+           Serial.print("MIFARE_Write() failed: ");
+           //Serial.println(mfrc522.GetStatusCodeName(status));
+           return 4;//return "4" as error message
+  }
+  Serial.println("block was written");
+}
+
+
+int readBlock(int blockNumber, byte arrayAddress[]) 
+{
+  int largestModulo4Number=blockNumber/4*4;
+  int trailerBlock=largestModulo4Number+3;//determine trailer block for the sector
+
+  /*****************************************authentication of the desired block for access***********************************************************/
+  byte status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(mfrc522.uid));
+  //byte PCD_Authenticate(byte command, byte blockAddr, MIFARE_Key *key, Uid *uid);
+  //this method is used to authenticate a certain block for writing or reading
+  //command: See enumerations above -> PICC_CMD_MF_AUTH_KEY_A = 0x60 (=1100000),    // this command performs authentication with Key A
+  //blockAddr is the number of the block from 0 to 15.
+  //MIFARE_Key *key is a pointer to the MIFARE_Key struct defined above, this struct needs to be defined for each block. New cards have all A/B= FF FF FF FF FF FF
+  //Uid *uid is a pointer to the UID struct that contains the user ID of the card.
+  if (status != MFRC522::STATUS_OK) {
+         Serial.print("PCD_Authenticate() failed (read): ");
+         //Serial.println(mfrc522.GetStatusCodeName(status));
+         return 3;//return "3" as error message
+  }
+  //it appears the authentication needs to be made before every block read/write within a specific sector.
+  //If a different sector is being authenticated access to the previous one is lost.
+
+
+  /*****************************************reading a block***********************************************************/
+        
+  byte buffersize = 18;//we need to define a variable with the read buffer size, since the MIFARE_Read method below needs a pointer to the variable that contains the size... 
+  status = mfrc522.MIFARE_Read(blockNumber, arrayAddress, &buffersize);//&buffersize is a pointer to the buffersize variable; MIFARE_Read requires a pointer instead of just a number
+  if (status != MFRC522::STATUS_OK) {
+          Serial.print("MIFARE_read() failed: ");
+          //Serial.println(mfrc522.GetStatusCodeName(status));
+          return 4;//return "4" as error message
+  }
+  Serial.println("block was read");
 }
